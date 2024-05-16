1bb62b3ef37343949f6f23ea30caec8e4a4bbe58
edulimastj <edulima@stj.jus.br>
2024-05-16 08:31:45 -0300

    Figuras dos produtos

--- js/index.js
+++ js/index.js
@@ -20,3 +20,56 @@ var rodape =
 
 document.getElementById('divRodape').innerHTML = '';
 document.getElementById('divRodape').innerHTML = rodape;    
+
+var array = [{
+    produto: "Notebook HP",
+    preco: 2249.00,
+    estoque: 13, 
+    imagem: "computador1.jpg"   
+},
+{
+    produto: "Notebook Samsung",
+    preco: 2199.00,
+    estoque: 58,  
+    imagem: "computador2.jpg"  
+},
+{
+    produto: "Notebook Dell",
+    preco: 2499.00,
+    estoque: 77,    
+    imagem: "computador3.jpg"
+},
+{
+    produto: "Notebook Lenovo",
+    preco: 2375.00,
+    estoque: 95,  
+    imagem: "computador4.jpg"  
+},
+{
+    produto: "Notebook Sony Vaio",
+    preco: 2999.00,
+    estoque: 43,   
+    imagem: "computador5.jpg" 
+}
+];
+document.getElementById('divProdutos').innerHTML = '';
+for (i = 0; i < array.length; i++)
+{
+    var produto =
+    `<div class="card">
+        <div class="card-capa">
+            <img src="../img/${array[i].imagem}" />
+        </div>
+        <div class="card-corpo">
+            <div class="card-corpo-produto">${array[i].produto}</div> 
+            <div>
+                Preço: <b> R$ ${array[i].preco.toFixed(2)} </b> <br>
+                Estoque: <b> ${array[i].estoque} </b> <br>
+            </div>
+        </div>
+        <div class="card-add-carrinho">
+            <i class="fa-solid fa-cart-plus"></i> Adicionar ao Carrinho
+        </div>
+    </div>`;
+    document.getElementById('divProdutos').innerHTML += produto;
+}
