6c3c5f91de768eb9cc2a9c7519d695edd7c5999d
uendelrocha <uendelrocha@gmail.com>
2024-06-09 02:08:29 -0300

    Atualização
    Produtos, estilos e script atualizados.
    - Adicionados 60 produtos seguindo o padrão da marca Orange
    - Ajustados estilos da página principal (cards)
    - Criado uma funcionalidade que avisa por balão ao adicionar o item ao carrinho

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,10 @@
 let produtos = [];
 let carrinho = [];
+let root_imagem = "../img/estoque/"
+const formatter = new Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL'
+})
 
 function loadProdutosFromStorage() {
     const storedProdutos = localStorage.getItem('produtos');
@@ -53,24 +58,69 @@ function renderProdutos() {
     if (produtosContainer) {
         produtosContainer.innerHTML = '';
         produtos.forEach(produto => {
-            const produtoElement = document.createElement('div');
-            produtoElement.className = 'produto';
-            produtoElement.innerHTML = `
-                <img src="${produto.imagem}" alt="${produto.produto}">
-                <h3>${produto.produto}</h3>
-                <p>R$ ${produto.preco.toFixed(2)}</p>
-                <p>Estoque: ${produto.estoque}</p>
-                <button onclick="adicionarAoCarrinho(${produto.id})" ${produto.estoque === 0 ? 'disabled' : ''}>Adicionar ao Carrinho</button>
-                
-            `;
-            produtosContainer.appendChild(produtoElement);
+            const elementoCard = document.createElement('div');
+            elementoCard.className = 'produto-card';
+            
+            const elementoImagem = document.createElement('div');
+            elementoImagem.className = 'produto-imagem';
+
+            const elementoNome = document.createElement('div');
+            elementoNome.className = 'produto-nome';
+
+            const elementoPreco = document.createElement('div');
+            elementoPreco.className = 'produto-preco';
+
+            const elementoEstoque = document.createElement('div');
+            elementoEstoque.className = 'produto-estoque';
+
+            const elementoAdicionarCarrinho = document.createElement('div');
+            elementoAdicionarCarrinho.className = 'produto-adicionar';
+
+            const elementoComprar = document.createElement('div');
+            elementoComprar.className = 'produto-comprar';
+
+            prod_imagem = root_imagem.concat(produto.imagem);
+            elementoImagem.innerHTML  = `<img class="imagem" src="${prod_imagem}" alt="${produto.produto}">`;
+            elementoNome.innerHTML    = `<h3 class="nome">${produto.produto}</h3>`;
+            elementoPreco.innerHTML   = `<p class="preco">${formatter.format(produto.preco)}</p>`;
+            elementoEstoque.innerHTML = `<p class="estoque">${produto.estoque} disponíveis</p>`;
+            elementoAdicionarCarrinho.innerHTML = `<button onclick="adicionarAoCarrinho(${produto.id})" ${produto.estoque === 0 ? 'disabled' : ''} class="botaoAdicionar">Adicionar ao Carrinho</button>`;
+            elementoComprar.innerHTML = `<button onclick="comprar(${produto.id})" ${produto.estoque === 0 ? 'disabled' : ''} class="botaoComprar">Comprar</button>`;
+            
+            elementoCard.appendChild(elementoImagem);
+            elementoCard.appendChild(elementoNome);
+            elementoCard.appendChild(elementoPreco);
+            elementoCard.appendChild(elementoEstoque);
+            elementoCard.appendChild(elementoAdicionarCarrinho);
+            elementoCard.appendChild(elementoComprar)
+            
+            produtosContainer.appendChild(elementoCard);
         });
     }
 }
 
+function showBalloon() {
+    // Create balloon element
+    const balloon = document.createElement('div');
+    balloon.textContent = 'Produto adicionado ao carrinho!';
+    balloon.style.position = 'fixed';
+    balloon.style.bottom = '20px';
+    balloon.style.right = '20px';
+    balloon.style.backgroundColor = 'lightgreen';
+    balloon.style.padding = '10px';
+    balloon.style.borderRadius = '5px';
+
+    // Append balloon to body
+    document.body.appendChild(balloon);
+
+    // Remove balloon after 1 second
+    setTimeout(() => {
+        balloon.remove();
+    }, 1000);
+};
 
 // Add product to cart
-function adicionarAoCarrinho(id) {
+function adicionarAoCarrinho(id, mostrarBalao = true) {
     const produto = produtos.find(p => p.id === id);
     if (produto && produto.estoque > 0) {
         produto.estoque--;
@@ -85,9 +135,17 @@ function adicionarAoCarrinho(id) {
         // Save updated cart to sessionStorage
         saveCarrinho();
         // Redirect to the cart page
-        window.location.href = '../carrinho/index.html';
+
+        if (mostrarBalao) {
+            showBalloon();
+        }
     }
 }
+
+function comprar(id) {
+    adicionarAoCarrinho(id, false);
+    window.location.href = '../carrinho/index.html';
+    }
 
 // Remove produto from carrinho
 function removerDoCarrinho(id) {
