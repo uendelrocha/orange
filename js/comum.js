ccab0659dce1693b92849264765b7334045cdd8a
uendelrocha <uendelrocha@gmail.com>
2024-06-13 12:50:21 -0300

    Comentário
    Apenas um comentário indicando o menu responsivo em comum.js

--- js/comum.js
+++ js/comum.js
@@ -105,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
         renderCarrinho();
     }
 
+    // Menu responsivo
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
     menuToggle.addEventListener('click', () => {
